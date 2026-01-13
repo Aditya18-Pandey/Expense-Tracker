df7d198dc5a845564d4da8036848997d3496e829
Aditya Pandey <pan.aditya11.22@gmail.com>
2026-01-13 14:48:49 +0530

    Added a cancel button

--- script.js
+++ script.js
@@ -2,108 +2,174 @@
 const form = document.querySelector("#expense-form");
 const expenseList = document.querySelector("#Expense-list");
 
+const submitBtn = document.querySelector("#submit-btn");
+const cancelbtn = document.querySelector("#cancel-btn");
 const titleInput = document.querySelector("#title");
 const amountInput = document.querySelector("#amount");
 const categoryInput = document.querySelector("#category");
 const dateInput = document.querySelector("#date");
-const totalEl= document.querySelector("#total");
+const totalEl = document.querySelector("#total");
 
 //Storage Container
 let expenses = loadData();
-
-
+let editID = null;
 
 //function For loading Existing Data
-function loadData(){
-    const storedData = localStorage.getItem("expenses");
-    return storedData?JSON.parse(storedData) : [];
+function loadData() {
+  const storedData = localStorage.getItem("expenses");
+  return storedData ? JSON.parse(storedData) : [];
 }
 
 //Save Function
-function saveData(){
-    localStorage.setItem("expenses",JSON.stringify(expenses));
+function saveData() {
+  localStorage.setItem("expenses", JSON.stringify(expenses));
 }
 
-function generateID(){
-    return Date.now().toString();
+function generateID() {
+  return Date.now().toString();
 }
 //function addExpense
-function addExpense(title,amount,category,date){
-    let expense = {
-        id: generateID(),
-        title:title,
-        amount:amount,
-        category:category,
-        date: date
-    }
-    expenses.push(expense);
-    saveData();
+function addExpense(title, amount, category, date) {
+  let expense = {
+    id: generateID(),
+    title: title,
+    amount: amount,
+    category: category,
+    date: date,
+  };
+  expenses.push(expense);
+  saveData();
 }
 //function deleteExpense via ID and filter
-function deleteExpense(id){
-    expenses = expenses.filter(expense => expense.id !== id);
-    saveData();
-}
-function calculateTotal(){
-    let total = 0;
-    expenses.forEach(expense => {
-        total +=expense.amount;
-    });
-    return total;
+function deleteExpense(id) {
+  expenses = expenses.filter((expense) => expense.id !== id);
+  saveData();
 }
 
-//Adding click handler events
-form.addEventListener("submit", function(e){
-    e.preventDefault();  
-    console.log("Form Submitted");
+function startEdit(id) {
+  const expense = expenses.find((exp) => exp.id === id);
 
-    const title = titleInput.value.trim();
-    const amount = parseFloat(amountInput.value);
-    const category = categoryInput.value;
-    const date = dateInput.value;
+  submitBtn.textContent = "Update Expense";
+  cancelbtn.hidden = false;
+  titleInput.value = expense.title;
+  amountInput.value = expense.amount;
+  categoryInput.value = expense.category;
+  dateInput.value = expense.date;
+  
+  editID = id;
+  renderExpense();
+}
 
-    //for checking if there's any empty field
-    if(!title|| amount <=0 || isNaN(amount)) return;
+function cancelEdit(){
+  editID = null;
+  form.reset();
+  submitBtn.textContent = "Add Expense";
+  cancelbtn.hidden = true;
+  renderExpense();
+}
+cancelbtn.addEventListener("click", cancelEdit);
+
+function calculateTotal() {
+  let total = 0;
+  expenses.forEach((expense) => {
+    total += expense.amount;
+  });
+  return total;
+}
+
+function updateExpense(id, title, amount, category, date) {
+  const index = expenses.findIndex((exp) => exp.id === id);
+
+  expenses[index] = {
+    id,
+    title,
+    amount,
+    category,
+    date,
+  };
+  saveData();
+}
+
+//Adding click handler events for submitting of the form
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const title = titleInput.value.trim();
+  const amount = parseFloat(amountInput.value);
+  const category = categoryInput.value;
+  const date = dateInput.value;
+
+  //for checking if there's any empty field
+  if (!title || amount <= 0 || isNaN(amount)) return;
+
+  if (editID) {
+    updateExpense(editID, title, amount, category, date);
+  } else {
     addExpense(title, amount, category, date);
-    form.reset();
-    renderExpense();
-})
+  }
+  submitBtn.textContent = "Add Expense";
+  cancelbtn.hidden = true;
+  form.reset();
+  editID = null;
+  renderExpense();
+});
 
 //formatting date
 function formatDate(dateStr) {
-    return new Date(dateStr).toLocaleDateString();
+  return new Date(dateStr).toLocaleDateString();
 }
-function renderExpense(){
-    expenseList.innerHTML = "";
+//The rendering function
+function renderExpense() {
+  expenseList.innerHTML = "";
 
-    expenses.forEach(expense =>{
-        const li = document.createElement("li");
-        li.classList.add("expense-item");
-        
-        li.innerHTML = `
+  if(expenses.length === 0){
+    expenseList.innerHTML = `
+    <li class= "empty-state">
+      No expense yet. Add one above
+    </li>
+    `;
+    totalEl.textContent = "0";
+  }
+  expenses.forEach((expense) => {
+    const li = document.createElement("li");
+    li.classList.add("expense-item");
+
+    li.innerHTML = `
             <div class ="expense-info">
             <span>${expense.title} - ₹${expense.amount} </span>
             <small>${expense.category} • ${formatDate(expense.date)}</small>
             </div>
-            <button class="delete-btn" data-id = "${expense.id}">Delete</button>
+            <div class ="actions">
+            <button class = "edit-btn" data-id = "${expense.id}">Edit</button>
+            <button class="delete-btn" data-id = "${
+              expense.id
+            }">Delete</button></div>
+            
         `;
-        expenseList.appendChild(li);
-    })
-    renderTotal();
 
-}
-expenseList.addEventListener("click" , function(e){
-    if(e.target.tagName ==="BUTTON"){
-        const id = e.target.dataset.id;
-        deleteExpense(id);
-        renderExpense();
-        
+    if (expense.id === editID) {
+      li.classList.add("editing");
+      li.querySelector(".edit-btn").textContent = "Editing";
+      li.querySelector(".edit-btn").disabled = true;
     }
+    expenseList.appendChild(li);
+  });
+  renderTotal();
+}
+expenseList.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+
+  if (e.target.classList.contains("delete-btn")) {
+    deleteExpense(id);
+    renderExpense();
+  }
+  if (e.target.classList.contains("edit-btn")) {
+    startEdit(id);
+  }
 });
 
-function renderTotal(){
-    
-    totalEl.textContent = calculateTotal();
+function renderTotal() {
+  totalEl.textContent = calculateTotal();
 }
 
 renderExpense();
